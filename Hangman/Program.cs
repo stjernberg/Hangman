2e161945702f75f0c7b3587c5571c6248ed14082
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-09-07 14:09:33 +0200

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the hangman game. Please press 'Enter'  to start.");
+
+           while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine(" Press 'Enter' to start the game");
+            }
+
+            ChooseWord();
+        }
+
+        static void ChooseWord()
+        {
+            string[] words = new string[] { "car", "boat", "windsurfer", "automatic", "programming", "holiday", "christmas", "summer", "winter" };
+            Random rand = new Random();
+            int index = rand.Next(words.Length);
+            Console.WriteLine(words[index]);
+        }
+    }
+}
